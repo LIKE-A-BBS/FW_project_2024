99c8c0cdb540349a125f792b638a57c1c163f8a2
LIKE-A-BBS <129242529+LIKE-A-BBS@users.noreply.github.com>
2024-10-20 21:14:17 +0900

    timer bit need check

--- ARQ_FSMs/SAW/FSM_SAW.v
+++ ARQ_FSMs/SAW/FSM_SAW.v
@@ -3,24 +3,25 @@
 module FSM_SAW_transmitter
 #(
     parameter x = 10,
-    parameter tp = 3        //propagation time
+    parameter tp = 3,        // propagation time * 2 + alpha
+	parameter OUT_BW = 5
 )
 (
-    output reg [3:0] state, next_state,
-	output reg [4:0] out,                                          // send, rst_timer, copy, discard, add_timer,
-	input in,
-    input clk, rstn
+    output reg 	[3:0] 			state, next_state,
+	output reg 	[OUT_BW-1:0] 	out,				// make_frame, copy, send, rst_timer, stop_timer,
+	input 		[2:0] 			in,					// enable-packet, time-out bit, ACK
+    input 						clk, rstn
 );
 
 	//States
 	parameter S0 = 0, S1 = 1; // S0 = Ready, S1 = Blocking
-    parameter S01 = 2, S02 = 3, S03 = 4, S04 = 5;
-    parameter S11 = 6, S12 = 7, S13 = 8, S14 = 9;
+    parameter S01 = 2, S02 = 3, S03 = 4;
+    parameter S11 = 5, S12 = 6, S13 = 7, S14 = 8;
     /* 
     S0
     :Packet came from network layer
     Make a frame -> save a copy -> and send the frame -> Start the timer
-    S01          -> S02         -> S03                -> S04
+    			S01             S02         		 S03                
 
     S1
     :Time-out
@@ -39,25 +40,46 @@ module FSM_SAW_transmitter
 	always @(*) begin
 		case (state)
 			S0: begin
-				if (in == 1'b1) begin
-					next_state <= S1;
-					out <= 1'b1;
+				if (in[2] == 1'b1) begin
+					next_state <= S01;
+					out <= 5'b10000;
 				end
 				else begin
 					next_state <= S0;
-					out <= S0;
+					out <= {(OUT_BW){1'b0}};
 				end
+			end
+			S01: begin
+				next_state <= S02;
+				out <= 5'b01000;
+			end
+			S02: begin
+				next_state <= S03;
+				out <= 5'b00100;
+			end
+			S03: begin
+				next_state <= S1;
+				out <= 5'b00010;
 			end
 			S1: begin
-				if (in == 1'b1) begin
-					next_state <= S0;
-					out <= S0;
+				if (in[1] == 1'b1) begin	// time out
+					next_state <= S03;
+					out <= 5'b00100;
+				end
+				else if (in[0] == 1'b1) begin
+					next_state <= S11;
+					out <= 5'b00001;
 				end
 				else begin
 					next_state <= S1;
-					out <= S1;
+					out <= {(OUT_BW){1'b0}};
 				end
 			end
+			S11: begin
+				next_state <= S1;
+				out <= 5'b00010;
+			end
+			
             default: begin
                     next_state <= S0;
                     out <= S0;
--- ARQ_FSMs/SAW/TOP_SAW.v
+++ ARQ_FSMs/SAW/TOP_SAW.v
@@ -9,7 +9,7 @@ module TOP_SAW
     wire [1:0] ctrl;
 
     //Control
-    FSM_SAW fsm ();
+    FSM_SAW_transmitter fsm ();
 
     //Make frame
     CRC crc ();
